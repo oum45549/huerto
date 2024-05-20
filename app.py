be8c5e1cf795213ec13b2777ef3b07d3bafd0782
Dani <danielrobledo2001@gmail.com>
2024-05-20 15:34:10 +0200

    Todas las gráficas funcionan perfectamente. No se llena el CSV con datos nuevos

--- app.py
+++ app.py
@@ -28,11 +28,10 @@ columna_seleccionada = st.selectbox("Selecciona la columna para graficar:", list
 
 # Opciones de visualización
 opciones_visualizacion = {
-    'Todos los datos': data,
+    'Todos los tiempos': data,
     'Últimos 30 días': data[data['Fecha'] >= datetime.now() - timedelta(days=30)],
     'Mes actual': data[data['Fecha'].dt.month == datetime.now().month],
     'Últimos 7 días': data[data['Fecha'] >= datetime.now() - timedelta(days=7)],
-    'Semana actual': data[data['Fecha'].dt.isocalendar().week == datetime.now().isocalendar().week],
     'Día actual': data[data['Fecha'].dt.date == datetime.now().date()]
 }
 opcion_visualizacion = st.selectbox("Selecciona la opción de visualización:", list(opciones_visualizacion.keys()))
@@ -44,11 +43,17 @@ datos_visualizacion = opciones_visualizacion[opcion_visualizacion]
 st.title(f'Gráfico de {columna_seleccionada} - {opcion_visualizacion}')
 fig, ax = plt.subplots()
 
-# Verificar si la columna 'Hora' está presente en los datos
+# Verificar si la opción de visualización es para el día actual
 if opcion_visualizacion == 'Día actual':
-    datos_visualizacion['Hora'] = datos_visualizacion['Fecha'].dt.strftime('%H:%M:%S')
+    # Ordenar los datos cronológicamente
+    datos_visualizacion = datos_visualizacion.sort_values(by='Fecha')
+    # Formatear la fecha para mostrar solo la hora
+    datos_visualizacion['Hora'] = datos_visualizacion['Fecha'].dt.strftime('%H:%M')
     ax.plot(datos_visualizacion['Hora'], datos_visualizacion[columna_seleccionada])
     ax.set_xlabel('Hora')
+    # Ajustar las etiquetas del eje X para mostrar solo cada hora
+    x_ticks = datos_visualizacion['Hora'].iloc[::12]  # Cada 12*5 = 60 minutos = 1 hora
+    ax.set_xticks(x_ticks)
 else:
     ax.plot(datos_visualizacion['Fecha'], datos_visualizacion[columna_seleccionada])
     ax.set_xlabel('Fecha')
